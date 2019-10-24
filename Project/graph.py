58d93acd3b4b01c97b1d38d8587355c1882de6a6
Arton <arton-10@o2.pl>
2019-10-24 19:42:32 +0200

    Function stubs

--- Project/graph.py
+++ Project/graph.py
@@ -1,13 +1,24 @@
 class Graph:
     def __init__(self):
-        self.vertices = [Vertex()]
+        self._vertices = [Vertex()]
+
+    def printVertices(self):
+        print(_vertices)
+    
+    def addVertex(self):
+        self._vertices.append(Vertex())
+
+        pass
+
+    def clone(self):
+        return clone
 
 class Vertex:
     def __init__(self, list_of_edges = []):
-        self.edges = list_of_edges
+        self._edges = list_of_edges
 
 class Edge:
     def __init__(self, left_vertex, right_vertex, value):
-        self.vertices = (left_vertex, right_vertex)
-        self.value = value
+        self._vertices = (left_vertex, right_vertex)
+        self._value = value
 
--- Project/script.py
+++ Project/script.py
@@ -2,7 +2,9 @@ import sys
 import graph
 
 if __name__ == "__main__":
-    print("Please, type in number of steps and then values for edges: ")
-    steps_number = int(input())
-    edge_cost = list(map(int, input().strip().split()[:steps_number]))
-    print(edge_cost)
+    # print("Please, type in number of steps and then values for edges: ")
+    # steps_number = int(input())
+    # edge_cost = list(map(int, input().strip().split()[:steps_number]))
+    # print(edge_cost)
+    graph = graph.Graph()
+    print(graph.vertices)
