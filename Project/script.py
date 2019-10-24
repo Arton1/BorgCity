a61d6477317ce533c1365d6f0c742b2f8f2d030f
Arton <arton-10@o2.pl>
2019-10-24 15:50:59 +0200

    Create a project filesystem

---
+++ Project/script.py
@@ -0,0 +1,7 @@
+import sys
+
+if __name__ == "__main__":
+    print("Please, type in number of steps and then values for edges: ")
+    steps_number = int(input())
+    edge_cost = list(map(int, input().strip().split()[:steps_number]))
+    print(edge_cost)
