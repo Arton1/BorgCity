b6626522ce15b7e1ff946e79e6dbe782ffc4f0c5
Arton <arton-10@o2.pl>
2020-01-18 14:52:10 +0100

    Refactoring

--- Project/profiling.py
+++ Project/profiling.py
@@ -33,15 +33,23 @@ def profile_calculate_brute_force(steps_number, edge_costs, profiler):
 
 
 if __name__ == "__main__":
+    print("Please, type in number of algorythim that you want to calculate the value with")
+    print("1 - algorithm using iterative formulas")
+    print("2 - algorithm using recursive formulas")
+    print("3 - algorithm using Depth First Search")
+    number_of_algorithm = int(input())
     edge_costs = []
     for i in range(steps_number):
         edge_costs.append(1)
     profiler = cProfile.Profile()
     for step in range(1, steps_number+1):
         for calculation in range(amount_of_calculations):
-            profile_calculate_iteratively(step, edge_costs, profiler)
-            # profile_calculate_recursively(step, edge_costs, profiler)
-            # profile_calculate_brute_force(step, edge_costs, profiler)
+            if number_of_algorithm == 1:
+                profile_calculate_iteratively(step, edge_costs, profiler)
+            elif number_of_algorithm == 2:
+                profile_calculate_recursively(step, edge_costs, profiler)
+            else:
+                profile_calculate_brute_force(step, edge_costs, profiler)
             profiler.disable()
         stats = pstats.Stats(profiler)
         print(f"{step} : {stats.total_tt/amount_of_calculations}")
--- Project/script.py
+++ Project/script.py
@@ -2,12 +2,20 @@ from recursive_solution import calculate_recursively, calculate_iteratively
 from graph import calculate_brute_force
 
 if __name__ == "__main__":
+    print("Please, type in number of algorythim that you want to calculate the value with")
+    print("1 - algorithm using iterative formulas")
+    print("2 - algorithm using recursive formulas")
+    print("3 - algorithm using Depth First Search")
+    number_of_algorithm = int(input())
     print("Please, type in number of steps and then values for edges: ")
     steps_number = int(input())
     edge_cost = list(map(int, input().strip().split()[:steps_number]))
     if(len(edge_cost) != steps_number):
         print("Please, type in the same amount of values as there are steps")
     else:
-        print(calculate_recursively(steps_number, edge_cost))
-        print(calculate_iteratively(steps_number, edge_cost))
-        print(calculate_brute_force(steps_number, edge_cost))
+        if(number_of_algorithm == 1):
+            print(calculate_iteratively(steps_number, edge_cost))
+        elif (number_of_algorithm == 2):
+            print(calculate_recursively(stps_number, edge_cost))
+        else:
+            print(calculate_brute_force(steps_number, edge_cost))
